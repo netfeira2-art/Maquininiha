23339567f37f7f918e2d6bd104033587c1965064
netfeira2-art <netfeira2@gmail.com>
2026-09-23 13:51:22 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -219,6 +219,8 @@ class Login:
             
             
             else:
+                
+                st.session_state.usuario= 'ti@demarchibrasil.com.br'
                                         
                 with open(temp_path,'w') as file:
                     
